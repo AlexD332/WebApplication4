a65e4fba566d91b6d6f7a8ca78848c231fef9d02
Alexey Davydov <adavydov@senseilabs.com>
2022-01-10 23:25:46 -0500

    v3

--- WebApplication4/Controllers/WeatherForecastController.cs
+++ WebApplication4/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApplication4.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing3", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
